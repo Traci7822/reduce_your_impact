a62ade19fed4fd7533ed435151764f7442fffce8
Traci7822 <Tracir@gmail.com>
2017-02-11 19:04:58 -0700

    setup relationships and migrations

--- db/migrate/20170212015346_create_topics.rb
+++ db/migrate/20170212015346_create_topics.rb
@@ -2,7 +2,7 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :name
-
+      t.belongs_to :category
       t.timestamps null: false
     end
   end
--- db/migrate/20170212015406_create_components.rb
+++ db/migrate/20170212015406_create_components.rb
@@ -2,7 +2,7 @@ class CreateComponents < ActiveRecord::Migration
   def change
     create_table :components do |t|
       t.string :name
-
+      t.belongs_to :topic
       t.timestamps null: false
     end
   end
