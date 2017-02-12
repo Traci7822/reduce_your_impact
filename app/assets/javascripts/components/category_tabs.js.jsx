c6a2dd0a3f4d5e3a7161d3e0c0682d03c5e4fd74
Traci7822 <Tracir@gmail.com>
2017-02-12 01:16:54 -0700

    styling navbar

--- app/assets/javascripts/components/category_tabs.js.jsx
+++ app/assets/javascripts/components/category_tabs.js.jsx
@@ -9,14 +9,14 @@ const CategoryTabs = React.createClass({
     this.setState({key});
   },
 
-  // set selected category to 'class = "active"'
+  // set selected category to 'class = "active"' | https://codepen.io/amwill/pen/qOMyEp
   render() {
     return (
       <div>
         <ul className="nav nav-tabs" role="tablist">
           {this.props.categories.map(
             (category) =>
-              <li role="presentation">
+              <li role="presentation" key={category.id}>
                 <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">
                   {category.name}
                 </a>
