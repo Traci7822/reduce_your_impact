674d12f10753c90106142bb23755e94260a1d7db
Traci7822 <Tracir@gmail.com>
2017-02-16 05:49:15 -0700

    handle click sets tab class to active

--- app/assets/javascripts/components/category_tabs.js.jsx
+++ app/assets/javascripts/components/category_tabs.js.jsx
@@ -1,13 +1,17 @@
 const CategoryTabs = React.createClass({
+
   getInitialState() {
     return {
-      key: 'Waste'
+      key: 'Waste',
+      active: false,
+      // active: false,
     };
   },
 
   handleSelect(key) {
     this.setState({
       key: key.target.innerHTML,
+      active: true,
     });
   },
   //
@@ -16,22 +20,26 @@ const CategoryTabs = React.createClass({
   // //   this.props.handleSelect();
   // }
 
-
-
   // set selected category to 'class = "active"' | https://codepen.io/amwill/pen/qOMyEp
   render() {
     return (
       <div>
-        <ul className="nav nav-tabs" role="tablist">
-          {this.props.categories.map(
-            (category) =>
-              <li role="presentation" key={category.id}>
-                <a href="#profile" className={this.state.key} aria-controls="profile" role="tab" data-toggle="tab" onClick={this.handleSelect} >
-                  {category.name}
-                </a>
-              </li>
-            )}
-        </ul>
+        <div>
+          <ul className="nav nav-tabs" role="tablist">
+            {this.props.categories.map(
+              (category) =>
+                <li role="presentation" key={category.id}>
+                  <a href="#profile" className={this.state.key} aria-controls="profile" role="tab" data-toggle="tab" onClick={this.handleSelect} >
+                    {category.name}
+                  </a>
+                </li>
+              )}
+            </ul>
+
+            </div>
+            <div>
+              {this.state.active ? `${this.state.key} + active` : null}
+            </div>
       </div>
 
     );
